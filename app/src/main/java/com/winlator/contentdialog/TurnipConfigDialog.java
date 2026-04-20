73aa13e7c2f1117ec37d4974ef210ea9d06a45f4
on-that-tiger-mountain <15920418725@139.com>
2026-04-21 02:10:45 +0800

    change "useHWBuf" to false by default.

--- app/src/main/java/com/winlator/contentdialog/TurnipConfigDialog.java
+++ app/src/main/java/com/winlator/contentdialog/TurnipConfigDialog.java
@@ -27,7 +27,7 @@ public class TurnipConfigDialog extends ContentDialog {
         final CheckBox cbForceWaitForFences = findViewById(R.id.CBForceWaitForFences);
 
         KeyValueSet config = new KeyValueSet(anchor.getTag());
-        cbUseHWBuf.setChecked(config.getBoolean("useHWBuf", true));
+        cbUseHWBuf.setChecked(config.getBoolean("useHWBuf"));
         cbForceWaitForFences.setChecked(config.getBoolean("forceWaitForFences"));
         AppUtils.setSpinnerSelectionFromMemorySize(sMaxDeviceMemory, config.get("maxDeviceMemory", "0"));
 
@@ -47,7 +47,7 @@ public class TurnipConfigDialog extends ContentDialog {
     public static void setEnvVars(Context context, KeyValueSet config, EnvVars envVars) {
         String maxDeviceMemory = config.get("maxDeviceMemory", "0");
         if (!maxDeviceMemory.equals("0")) envVars.put("TU_OVERRIDE_HEAP_SIZE", maxDeviceMemory);
-        if (config.getBoolean("useHWBuf", true)) envVars.put("MESA_VK_WSI_USE_HWBUF", "1");
+        if (config.getBoolean("useHWBuf")) envVars.put("MESA_VK_WSI_USE_HWBUF", "1");
         if (config.getBoolean("forceWaitForFences")) envVars.put("MESA_VK_WSI_FORCE_WAIT_FOR_FENCES", "1");
     }
 }
