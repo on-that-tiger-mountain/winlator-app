e56f8f39408bbdf2491da0e838a4aa1f61342f0f
on-that-tiger-mountain <167862084+on-that-tiger-mountain@users.noreply.github.com>
2026-05-07 02:20:56 +0800

    更新 AboutDialog.java

--- app/src/main/java/com/winlator/contentdialog/AboutDialog.java
+++ app/src/main/java/com/winlator/contentdialog/AboutDialog.java
@@ -25,13 +25,21 @@ public class AboutDialog extends ContentDialog {
             ((TextView)findViewById(R.id.TVAppVersion)).setText(context.getString(R.string.version)+" "+pInfo.versionName);
 
             String creditsAndThirdPartyAppsHTML = String.join("<br />",
+                "Winlator 11.0 Unofficial Revision by (<a href=\"https://github.com/on-that-tiger-mountain/Winlator-11.0-Unofficial-Revision-by-tiger\">tiger</a>)",
+                "Winlator 11.0 Original Version by (<a href=\"https://github.com/brunodev85/winlator\">brunodev85</a>)",
                 "GLIBC Patches by (<a href=\"https://github.com/termux-pacman/glibc-packages\">Termux Pacman</a>)",
                 "Wine (<a href=\"https://www.winehq.org\">winehq.org</a>)",
                 "Box86/Box64 by <a href=\"https://github.com/ptitSeb\">ptitseb</a>",
                 "Mesa (Turnip/Zink/VirGL) (<a href=\"https://www.mesa3d.org\">mesa3d.org</a>)",
+                "MTDataFilesProvider by (<a href=\"https://github.com/ewt45/winlator-fork/commit/b86061357897af69d29cd0c5b54ea0d88d067e2b\">ewt45/bubu23456</a>)",
+                "zink-22.2.5-fix by (<a href=\"https://github.com/on-that-tiger-mountain/mesa3d-custom/tree/zink-22.2.5-fix\">tiger</a>)",
                 "DXVK (<a href=\"https://github.com/doitsujin/dxvk\">github.com/doitsujin/dxvk</a>)",
+                "DXVK-Sarek by (<a href=\"https://github.com/pythonlover02/DXVK-Sarek\">pythonlover02</a>)",
+                "DXVK-Sarek-Mali-Gpu-fix by (<a href=\"https://github.com/zeyadadev/DXVK-Sarek\">zeyadadev</a>)",
+                "DXVK-Sarek-pal4 by (<a href=\"https://github.com/on-that-tiger-mountain/dxvk/tree/DXVK-Sarek-pal4\">tiger</a>)",
                 "VKD3D (<a href=\"https://gitlab.winehq.org/wine/vkd3d\">gitlab.winehq.org/wine/vkd3d</a>)",
-                "CNC DDraw (<a href=\"https://github.com/FunkyFr3sh/cnc-ddraw\">github.com/FunkyFr3sh/cnc-ddraw</a>)"
+                "CNC DDraw (<a href=\"https://github.com/FunkyFr3sh/cnc-ddraw\">github.com/FunkyFr3sh/cnc-ddraw</a>)",
+                "(<a href=\"https://github.com/alexvorxx\">alexvorxx</a>) (Mods/Tips) and others."
             );
 
             TextView tvCreditsAndThirdPartyApps = findViewById(R.id.TVCreditsAndThirdPartyApps);
